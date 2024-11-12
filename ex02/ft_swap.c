df1f067e7a5c54717fd0fdee34f90e275d2e1dfe
Viorica Gabriela Hacman <vhacman@e3r6p2.42roma.it>
2024-11-12 12:31:52 +0100

    es02

---
+++ ex02/ft_swap.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vhacman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 11:02:58 by vhacman           #+#    #+#             */
+/*   Updated: 2024/11/12 12:30:13 by vhacman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	*a = 3;
+	*b = 5;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/*
+int	main(void)
+{
+	int prova1;
+	int prova2;
+
+	prova1 = 3;
+	prova2 = 5;
+
+	printf ("questi e prova1: %d\n", prova1);
+	printf ("questi e prova2: %d\n", prova2);
+	ft_swap (&prova1, &prova2);
+	printf ("questi e prova1: %d\n", prova1);
+        printf ("questi e prova2: %d\n", prova2);
+	return (0);
+}
+*/
