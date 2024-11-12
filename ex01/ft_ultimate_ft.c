9aa57351358db4bf1b3277703d853ff26b919f0d
Viorica Gabriela Hacman <vhacman@e3r6p2.42roma.it>
2024-11-12 15:51:48 +0100

    ex01

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vhacman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 15:13:34 by vhacman           #+#    #+#             */
+/*   Updated: 2024/11/12 15:50:34 by vhacman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <std.io>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*
+int	main(void)
+{
+	int 	numero_prova = 12;
+	int	*nbr0 = &numero_prova;
+	int	**nbr1 = &numero_prova0;
+	int	***nbr2 = &numero_prova1;
+	int	****nbr3 = &numero_prova2;
+	int	*****nbr4 = &numero_prova3;
+	int	******nbr5 = &numero_prova4;
+	int	*******nbr6 = &numero_prova5;
+	int	********nbr7 = &numero_prova6;
+	int	*********nbr = &numero_prova7;
+
+	ft_ultimate_ft(&numero_prova7);
+	printf("Value of nbr: %d\n, n", numero_prova);
+	return (0);
+	}
+
+}*/
