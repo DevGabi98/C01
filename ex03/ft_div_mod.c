afbb4f06ced408b9f20d0ed9855704b238b96ec3
Viorica Gabriela Hacman <vhacman@e3r7p2.42roma.it>
2024-11-14 15:24:28 +0100

    C01

---
+++ ex03/ft_div_mod.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vhacman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 14:57:40 by vhacman           #+#    #+#             */
+/*   Updated: 2024/11/14 15:10:16 by vhacman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+//#include <stdio.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	if ( b != 0)
+	{
+		*div = a / b;
+		*mod = a % b;
+	}
+	else
+	{
+		write(1, "Error: division by zero!\n", 23);
+	}
+}
+/*
+int	main(int)
+{
+	int	a = 10;
+	int	b = 3;
+	int	div;
+	int	mod;
+
+	ft_div_mod(a, b, &div, &mod);
+	printf("a: %d, b: %d, div: %d, mod: %d\n", a, b, div, mod);
+	return(0);
+}*/
